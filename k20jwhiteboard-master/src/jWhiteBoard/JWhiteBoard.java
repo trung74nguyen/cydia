46cb3443c40d3c09cc18379429658514ef315631
mintaz <minhle1508@gmail.com>
2015-12-01 05:44:05 +0700

    Fix: Change Name of Button
    sữa lỗi sai tên button

--- k20jwhiteboard-master/src/jWhiteBoard/JWhiteBoard.java
+++ k20jwhiteboard-master/src/jWhiteBoard/JWhiteBoard.java
@@ -272,10 +272,10 @@ public class JWhiteBoard extends ReceiverAdapter implements ActionListener, Chan
         drawPanel.setBackground(backgroundColor);
         subPanel=new JPanel();
         mainFrame.getContentPane().add("Center", drawPanel);
-        clearButton=new JButton("Clean");
+        clearButton=new JButton("Clear"); // Fixed : change name button
         clearButton.setFont(defaultFont);
         clearButton.addActionListener(this);
-        leaveButton=new JButton("Exit");
+        leaveButton=new JButton("Leave"); // Fixed : change name button
         leaveButton.setFont(defaultFont);
         leaveButton.addActionListener(this);
         subPanel.add("South", clearButton);
